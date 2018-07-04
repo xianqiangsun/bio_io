70c3d32bf45c6b304c9789b836c88a9d90a5bfa5
xianqiangsun <xianqiangsun@126.com>
2018-07-04 16:07:29 +0800

    add set up function

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import setup, Extension, find_packages
+import os
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(name='BioIO',
+      version='1.0.1',
+      description='Python Distribution Utilities',
+      author='Xianqiang Sun',
+      author_email='xianqiangleo@gmail.com',
+      url='github.com/xianqiangsun/bio_io',
+      packages=find_packages(),
+      package_dir={'bio_io': './BioIO'},
+      #package_data={
+      #            'Bio': ['database/*']
+      #            },
+      #packages=['quantitative'],
+
+      include_package_data=True,
+      install_requires=['numpy',
+                        'pandas',
+                        'scipy',
+                        'rdkit',
+                        'json',
+                        'matplotlib',
+                        'python-dateutil>=2.5.0',
+                        'Bio',
+                        'bioservices'
+                        ])
